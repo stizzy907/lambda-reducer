895c8cecc0f089bb84cb61af7bd7628414ea9477
Gary Moore <gmoore@ent.com>
2018-02-27 12:22:40 -0700

    Favicon working and bootstrap styling

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,5 +1,20 @@
 import React from 'react';
 
-const App = () => <div>Lambda Reducer</div>
+const App = () => (
+  <div>
+    <div className="navbar navbar-dark bg-primary">
+      <a className="navbar-brand" href="/">
+        Lambda Reducer
+      </a>
+    </div>
+    <div className="container">
+      <div className="row">
+        <div className="mt-3">
+          <h1>Lambda Reducer</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+)
 
 export default App;
